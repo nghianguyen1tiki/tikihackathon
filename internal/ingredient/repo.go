5309ae546b9eb23704ee8e2fd320d75e91586d3f
Hiep Van Viet <hiep.van1@tiki.vn>
2021-04-16 23:36:57 +0700

    Update ingredient

--- internal/ingredient/repo.go
+++ internal/ingredient/repo.go
@@ -2,6 +2,7 @@ package ingredient
 
 import (
 	"context"
+	"fmt"
 
 	"gorm.io/gorm"
 
@@ -12,6 +13,7 @@ type Repo interface {
 	Get(ctx context.Context, id int) (model.Ingredient, error)
 	Create(ctx context.Context, ingredient *model.Ingredient) error
 	List(ctx context.Context, page int, limit int, filter map[string]interface{}) ([]*model.Ingredient, error)
+	ListByIDs(ctx context.Context, ids []int64) ([]*model.Ingredient, error)
 }
 
 var _ Repo = (*repo)(nil)
@@ -36,9 +38,21 @@ func (r *repo) Create(ctx context.Context, ingredient *model.Ingredient) error {
 
 func (r *repo) List(ctx context.Context, page int, limit int, filter map[string]interface{}) ([]*model.Ingredient, error) {
 	var records []*model.Ingredient
-	name, _ := filter["name"].(string)
+	name := fmt.Sprintf("%v", filter["name"])
 
 	offset := (page - 1) * limit
-	err := r.db.WithContext(ctx).Where("name LIKE ?", "%"+name+"%").Order("id DESC").Limit(limit).Offset(offset).Find(&records).Error
+	err := r.db.WithContext(ctx).
+		Joins("INNER JOIN tiki_cat on ingredients.tiki_cate_id = tiki_cat.id").
+		Where("name LIKE ?", "%"+name+"%").Order("id DESC").Limit(limit).Offset(offset).Find(&records).Error
+	return records, err
+}
+
+func (r *repo) ListByIDs(ctx context.Context, ids []int64) ([]*model.Ingredient, error) {
+	var records []*model.Ingredient
+
+	err := r.db.WithContext(ctx).
+		Joins("INNER JOIN tiki_cat on ingredients.tiki_cate_id = tiki_cat.id").
+		Where("ingredients.id IN ?", ids).
+		Order("id DESC").Find(&records).Error
 	return records, err
 }
--- internal/model/ingredient.go
+++ internal/model/ingredient.go
@@ -3,9 +3,16 @@ package model
 type Ingredient struct {
 	ID   int
 	Name string `gorm:"unique"`
+	CatID string
 }
 
 type Unit struct {
 	ID   int
 	Name string `gorm:"unique"`
+}
+
+type TikiCate struct {
+	Id int
+	Name string
+	CateID string
 }
